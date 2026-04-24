6ae2355f38da83d12c26042f6af423ac3d88108a
Vlad <vlad>
2026-04-25 01:41:30 +0500

    chore: sync changes in ./npm/publication-foundation/publication-photon

--- src/sdk.ts
+++ src/sdk.ts
@@ -9,22 +9,48 @@ export const publicationPhotonSiteFrameExtension: PhotonSiteFrameExtension =
 		label: "Publication",
 		order: 40,
 		header: {
-			utilityLinks: [
-				{ id: "publication:blog", label: "Blog", href: "/blog", order: 40 },
-				{ id: "publication:news", label: "News", href: "/news", order: 50 },
-			],
-		},
-		footer: {
-			navigationColumns: [
-				{
-					id: "publication:footer",
-					title: "Publication",
-					order: 40,
+			slots: {
+				utility: {
 					links: [
-						{ id: "publication:footer-blog", label: "Blog", href: "/blog" },
-						{ id: "publication:footer-news", label: "News", href: "/news" },
+						{
+							id: "publication:blog",
+							label: "Blog",
+							href: "/blog",
+							order: 40,
+						},
+						{
+							id: "publication:news",
+							label: "News",
+							href: "/news",
+							order: 50,
+						},
 					],
 				},
-			],
+			},
+		},
+		footer: {
+			slots: {
+				navigation: {
+					navigationColumns: [
+						{
+							id: "publication:footer",
+							title: "Publication",
+							order: 40,
+							links: [
+								{
+									id: "publication:footer-blog",
+									label: "Blog",
+									href: "/blog",
+								},
+								{
+									id: "publication:footer-news",
+									label: "News",
+									href: "/news",
+								},
+							],
+						},
+					],
+				},
+			},
 		},
 	});
